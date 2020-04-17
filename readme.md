51a09c4ed2740815b67fa87224e8215d98129940
getvince <37116566+getvince@users.noreply.github.com>
2020-04-17 18:32:27 +0200

    Inital upload
    initial release pretty ghetto

--- readme.md
+++ readme.md
@@ -4,5 +4,6 @@ Currently in pre-alpha version 0.0.1
 
 Based on the:
 	- [Firefly III CSV Importer Docker image](https://github.com/firefly-iii/csv-importer-docker)
+	
 	- [firefly-iii-fints-importer](https://github.com/bnw/firefly-iii-fints-importer)
 	
---
+++ scripts/entrypoint.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+echo "Now in entrypoint.sh (v0.0.1) for fints importer."
+echo "Please wait for the container to start..."
+
+# make sure the correct directories exists (suggested by @chrif):
+mkdir -p $HOMEPATH/storage/app/public
+mkdir -p $HOMEPATH/storage/build
+mkdir -p $HOMEPATH/storage/database
+mkdir -p $HOMEPATH/storage/debugbar
+mkdir -p $HOMEPATH/storage/export
+mkdir -p $HOMEPATH/storage/framework/cache/data
+mkdir -p $HOMEPATH/storage/framework/sessions
+mkdir -p $HOMEPATH/storage/framework/testing
+mkdir -p $HOMEPATH/storage/framework/views/twig
+mkdir -p $HOMEPATH/storage/framework/views/v1
+mkdir -p $HOMEPATH/storage/framework/views/v2
+mkdir -p $HOMEPATH/storage/logs
+mkdir -p $HOMEPATH/storage/upload
+
+echo "We got pretty far...."
+
+# make sure we own the volumes:
+chown -R www-data:www-data -R $HOMEPATH/storage
+chmod -R 775 $HOMEPATH/storage
+
+# remove any lingering files that may break upgrades:
+rm -f $HOMEPATH/storage/logs/laravel.log
+
+echo "now the new part"
+
+php -S 0.0.0.0:5085 app/index.php
